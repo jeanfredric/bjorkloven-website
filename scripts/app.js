46dd956f6f799802b38d21c9c6841d748b0fce4c
Fredric <jeanfredric@MacBook-Pro-som-tillhor-Fredric.local>
2020-12-14 09:50:53 +0100

    Hero article carousel implemented

--- scripts/app.js
+++ scripts/app.js
@@ -72,4 +72,73 @@ $(document).ready(function(){
     setBoxText();
   });
 
+
+  /* Hamburger menu */
+  let bg = $(".hamburger-bg");
+  let menuOptions = $("#menu");
+  let menuNav = $("#menu-nav");
+
+  $("#hamburger").click(function() {
+    
+    bg.show();
+    bg.animate({
+        height: "100vh",
+        width: "100vw"
+      },350, '',(function(){
+        
+        menuOptions.fadeIn(1000);
+        menuNav.fadeIn(500);
+
+        disableScroll();
+      }));
+    });
+
+    menuOptions.click(function() {
+      menuOptions.hide();
+      menuNav.hide();
+      bg.hide();
+      enableScroll();
+    });
+
+});
+
+/* Hero news carousel */
+
+const slider = document.querySelector('.slider');
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+const indicatorsParent = document.querySelector('.controls ul');
+const indicators = document.querySelectorAll('.controls li');
+
+let sectionIndex = 0;
+const nrOfSections = 2;
+
+function setIndex() {
+  document.querySelector('.controls .selected').classList.remove('selected');
+  slider.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
+}
+
+indicators.forEach((indicator, i) => {
+  indicator.addEventListener('click', () => {
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicator.classList.add('selected');
+    slider.style.transform = 'translate(' + (i) * -50 + '%)';
+    sectionIndex = i;
+  })
+});
+
+rightArrow.addEventListener('click', () => {
+  sectionIndex = (sectionIndex < (nrOfSections - 1) ? sectionIndex + 1 : 0);
+  document.querySelector('.controls .selected').classList.remove('selected');
+  indicatorsParent.children[sectionIndex].classList.add('selected');
+  slider.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
+
+});
+
+leftArrow.addEventListener('click', () => {
+  sectionIndex = (sectionIndex > 0 ? sectionIndex - 1: (nrOfSections - 1));
+  document.querySelector('.controls .selected').classList.remove('selected');
+  indicatorsParent.children[sectionIndex].classList.add('selected');
+  slider.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
+  
 });
